62eb773a4c0e47a0f53545a381a6cdb369a8e229
Hema-karthi <hemadhanabalkarthi@gmail.com>
2026-06-18 11:32:49 +0530

    Add files via upload

---
+++ Profit.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+class Profit{
+public static void main(String []args){
+
+int cost_price=6500;
+int selling_price=9500;
+
+float profit=selling_price - cost_price;
+
+float profitPercentage=(profit/cost_price)*100;
+System.out.println("profit percentage ="+profitPercentage+"%");
+}
+}
+
+   

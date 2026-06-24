90831e67690ec65b38ad78fd9898ebe2a7ba8a7f
Hema-karthi <hemadhanabalkarthi@gmail.com>
2026-06-24 11:36:21 +0530

    Add files via upload

---
+++ Nestedif3.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+class Nestedif3{
+public static void main(String []args){
+
+Scanner s= new Scanner(System.in);
+
+
+ String input=s.nextLine();
+System.out.println("click 'yes' or 'no'");
+
+
+while(input.equals("yes"))
+{
+  System.out.println("Do you want to continue");
+  input=s.nextLine();
+   
+
+}
+System.out.println("Thankyou");
+}
+}
+  
+   
+
+
+
+ 

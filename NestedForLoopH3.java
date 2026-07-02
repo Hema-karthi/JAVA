804f7438c8bb539d99a3c1a92bb09810b167a6e1
Hema-karthi <hemadhanabalkarthi@gmail.com>
2026-07-02 09:34:42 +0530

    Add files via upload

---
+++ NestedForLoopH3.java
@@ -0,0 +1,21 @@
+        
+import java.io.*;
+ class NestedForLoopH3{
+     public static void main(String []args){
+      
+      int z=1;
+    for(int i=1; i<=3; i++)
+         {
+       for(int j=3; j>=i; j--)
+              {
+          System.out.print(z+" ");
+             z++;
+              }
+          System.out.print("\n");
+         }
+}
+}
+
+
+
+       
